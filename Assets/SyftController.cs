4e0a0444c3b7ec7a10c83241d5b186ee42dbac79
Gavin Uhma <gavinmail@gmail.com>
2017-11-10 23:17:40 -0400

    train a basic nn on the gpu

--- Assets/SyftController.cs
+++ Assets/SyftController.cs
@@ -20,6 +20,10 @@ public class FloatTensor {
 	private int ScalarMultMain;
 	private int ElementwiseMultMain;
 	private int ElementwiseSubtractMain;
+	private int SigmoidMatrixMultiply;
+	private int MultiplyDerivative;
+	private int AddMatrixMultiply;
+	private int ResetWeights;
 
 	public FloatTensor(float[] _data, int[] _shape, ComputeShader _shader)
 	{
@@ -48,6 +52,10 @@ public class FloatTensor {
 		ScalarMultMain = shader.FindKernel ("ScalarMultMain");
 		ElementwiseMultMain = shader.FindKernel ("ElementwiseMultMain");
 		ElementwiseSubtractMain = shader.FindKernel ("ElementwiseSubtractMain");
+		SigmoidMatrixMultiply = shader.FindKernel ("SigmoidMatrixMultiply");
+		MultiplyDerivative = shader.FindKernel ("MultiplyDerivative");
+		AddMatrixMultiply = shader.FindKernel ("AddMatrixMultiply");
+		ResetWeights = shader.FindKernel ("ResetWeights");
 
 	}
 
@@ -96,7 +104,7 @@ public class FloatTensor {
 	}
 
 	public void inline_elementwise_subtract(FloatTensor other) {
-		Debug.LogFormat("<color=blue>FloatTensor.inline_elementwise_subtract data_on_gpu: {0}</color>", data_on_gpu);
+		//Debug.LogFormat("<color=blue>FloatTensor.inline_elementwise_subtract data_on_gpu: {0}</color>", data_on_gpu);
 
 		if (size () == other.size ()) {
 			if (data_on_gpu && other.data_is_on_gpu ()) {
@@ -105,7 +113,7 @@ public class FloatTensor {
 				shader.SetBuffer (ElementwiseSubtractMain, "data_c", data_buffer);
 				shader.SetBuffer (ElementwiseSubtractMain, "data_d", other.data_buffer);
 
-				shader.Dispatch(ElementwiseSubtractMain, 1, 1, 1);
+				shader.Dispatch(ElementwiseSubtractMain, _size, 1, 1);
 
 			} else if (!data_on_gpu && !other.data_is_on_gpu ()) {
 				for (int i = 0; i < _size; i++) {
@@ -117,6 +125,73 @@ public class FloatTensor {
 		} else {
 			Debug.Log("Tensors do not have the same number of elements!");
 		}
+	}
+
+	public void init_sigmoid_matrix_multiply(FloatTensor tensor_1) {
+
+		Dimensions[] dim = new Dimensions[] {
+			new Dimensions (tensor_1.shape.Length, tensor_1.shape [0])
+		};
+
+		ComputeBuffer dim_buffer = new ComputeBuffer (dim.Length, dim[0].stride());
+		dim_buffer.SetData (dim);
+		shader.SetBuffer (SigmoidMatrixMultiply, "dimensions_a", dim_buffer);
+	}
+
+	public void sigmoid_matrix_multiply(FloatTensor tensor_1, FloatTensor tensor_2) {
+		//Debug.LogFormat("<color=blue>FloatTensor.sigmoid_matrix_multiply data_on_gpu: {0}</color>", data_on_gpu);
+		shader.SetBuffer (SigmoidMatrixMultiply, "data_e", data_buffer);
+		shader.SetBuffer (SigmoidMatrixMultiply, "data_f", tensor_1.data_buffer);
+		shader.SetBuffer (SigmoidMatrixMultiply, "data_g", tensor_2.data_buffer);
+		shader.Dispatch (SigmoidMatrixMultiply, _size, 1, 1);
+	}
+
+	public void multiply_derivative(FloatTensor tensor_1) {
+		//Debug.LogFormat("<color=blue>FloatTensor.multiply_derivative data_on_gpu: {0}</color>", data_on_gpu);
+		shader.SetBuffer (MultiplyDerivative, "data_h", data_buffer);
+		shader.SetBuffer (MultiplyDerivative, "data_i", tensor_1.data_buffer);
+		shader.Dispatch (MultiplyDerivative, _size, 1, 1);
+	}
+
+	public struct Dimensions {
+		public int rows, columns;
+
+		public Dimensions (int _rows, int _columns) {
+			rows = _rows;
+			columns = _columns;
+		}
+
+		public int stride() {
+			return 2 * sizeof(int);
+		}
+	}
+
+	public void init_add_matrix_multiply(FloatTensor tensor_1) {
+
+		Dimensions[] dim = new Dimensions[] {
+			new Dimensions (tensor_1.shape.Length, tensor_1.shape [0])
+		};
+
+		ComputeBuffer dim_buffer = new ComputeBuffer (dim.Length, dim[0].stride());
+		dim_buffer.SetData (dim);
+		shader.SetBuffer (AddMatrixMultiply, "dimensions_b", dim_buffer);
+	}
+
+	public void add_matrix_multiply(FloatTensor tensor_1, FloatTensor tensor_2) {
+		//Debug.LogFormat("<color=blue>FloatTensor.add_matrix_multiply data_on_gpu: {0}</color>", data_on_gpu);
+		shader.SetBuffer (AddMatrixMultiply, "data_j", data_buffer);
+		shader.SetBuffer (AddMatrixMultiply, "data_k", tensor_1.data_buffer); //d
+		shader.SetBuffer (AddMatrixMultiply, "data_l", tensor_2.data_buffer);
+		shader.Dispatch (AddMatrixMultiply, _size, 1, 1);
+	}
+
+	public void init_weights(FloatTensor save_tensor) {
+		shader.SetBuffer (ResetWeights, "weights", data_buffer);
+		shader.SetBuffer (ResetWeights, "original_weights", save_tensor.data_buffer);
+	}
+
+	public void reset_weights() {
+		shader.Dispatch (ResetWeights, _size, 1, 1);
 	}
 
 	public bool data_is_on_gpu() {
@@ -213,6 +288,9 @@ public class SyftController {
 		shader = _shader;
 
 		tensors = new List<FloatTensor>();
+
+		test_inline_elementwise_subtract ();
+
 	}
 
 	public void processMessage(string message) {
@@ -230,10 +308,10 @@ public class SyftController {
 				fdata [i] = float.Parse (splittedStrings [i + 1]);
 			}
 
-			int[] fsize = new int[1];
-			fsize [0] = splittedStrings.Length - 1;
+			int[] fshape = new int[1];
+			fshape [0] = splittedStrings.Length - 1;
 
-			FloatTensor x = new FloatTensor (fdata, fsize, shader);
+			FloatTensor x = new FloatTensor (fdata, fshape, shader);
 
 			tensors.Add (x);
 
@@ -296,5 +374,25 @@ public class SyftController {
 
 	}
 
+	void test_inline_elementwise_subtract () {
+
+		float[] xdata = new float[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+		int[] xshape = new int[] { 3, 3, 3, 3 };
+		FloatTensor xtensor = new FloatTensor (xdata, xshape, shader);
+		xtensor.gpu ();
+
+		//float[] ydata = new float[] { 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1 };
+		float[] ydata = new float[] { 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+		int[] yshape = new int[] { 3, 3, 3, 3 };
+		FloatTensor ytensor = new FloatTensor (ydata, yshape, shader);
+		ytensor.gpu ();
+
+		xtensor.inline_elementwise_subtract (ytensor);
+
+		xtensor.cpu ();
+		ytensor.cpu ();
+
+		Debug.LogFormat ("<color=green>SyftController.test_inline_elementwise_subtract: {0}</color>", string.Join (",", xtensor.data));
+	}
+
 }
-	
