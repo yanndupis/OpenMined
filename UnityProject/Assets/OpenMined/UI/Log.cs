0b08e309b9ca4e2c5ecaa8cb9b5509da20420cd1
Gavin Uhma <gavinuhma@users.noreply.github.com>
2017-12-07 00:54:41 -0800

    log to the ui (#156)
    Great job!!!

---
+++ UnityProject/Assets/OpenMined/UI/Log.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+namespace OpenMined.UI
+{
+    public class Log : MonoBehaviour
+    {
+        public Text logText;
+
+        public static Log Instance;
+
+        void Awake()
+        {
+            Instance = this;
+        }
+
+        void Start()
+        {
+            this.ClearText();
+            this.SetText("<color=white>Started logging...</color>");
+        }
+
+        void OnEnable()
+        {
+            Application.logMessageReceived += HandleLog;
+        }
+
+        void OnDisable()
+        {
+            Application.logMessageReceived -= HandleLog;
+        }
+
+        void HandleLog(string logString, string stackTrace, LogType type)
+        {
+            this.SetText(logString);
+        }
+
+        public void ClearText()
+        {
+            logText.text = "";
+        }
+
+        public void SetText(string text)
+        {
+            //UnityEngine.Debug.LogFormat(text);
+            logText.text += text + "\n";
+        }
+
+        public static void LogFormat(string format, params object[] args)
+        {
+            OpenMined.UI.Log.Instance.SetText(String.Format(format, args));
+        }
+    }
+}
