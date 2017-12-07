a19804e183248285a3a7f040485efc896e908357
iamtrask <liamtrask@gmail.com>
2017-12-07 01:23:11 +0000

    Autograd improvements (#155)
    * improved autograd
    
    * added autograd hook
    
    * added division

--- UnityProject/Assets/OpenMined/Syft/Tensor/FloatTensor.Autograd.cs
+++ UnityProject/Assets/OpenMined/Syft/Tensor/FloatTensor.Autograd.cs
@@ -35,6 +35,22 @@ namespace OpenMined.Syft.Tensor
 			return true;
 		}
 
+		public void HookAutograd(ref FloatTensor result, ref FloatTensor x, string creation_op) {
+		
+			if (autograd) {
+
+				result.InitAutograd ();
+				result.creators.Add (this);
+				result.creators.Add (x);
+				result.creation_op = creation_op;
+
+				children.Add (result.Id, 0);
+				x.children.Add (result.Id, 0);
+
+			}
+		
+		}
+
 		public void Backward(FloatTensor grad = null, FloatTensor grad_origin=null) {
 
 			if (autograd) {
@@ -66,12 +82,20 @@ namespace OpenMined.Syft.Tensor
 				// only continue backpropping if all gradients (from children) are accounted for
 				// override waiting for children if "backprop" was called on this variable directly
 				if(this.creators != null && this.creators.Count > 0 && (grad_origin == null || AllChildrenGradsAccountedFor())) {
-					if (creation_op == "add") {
+					if (creation_op == "add_elem") {
+
 						creators [0].Backward (grad, this);
 						creators [1].Backward (grad, this);
-					} else if (creation_op == "mul") {
-						creators [0].Backward (grad.Mul(creators[1]), this);
-						creators [1].Backward (grad.Mul(creators[0]), this);
+
+					} else if (creation_op == "mul_elem") {
+						creators [0].Backward (grad.Mul (creators [1]), this);
+						creators [1].Backward (grad.Mul (creators [0]), this);
+					} else if (creation_op == "div_elem") {
+						creators [0].Backward (grad.Div (creators [1]), this);
+						creators [1].Backward (grad.Div (creators [0]), this);
+					} else if (creation_op == "sub_elem") {
+						creators [0].Backward (grad, this);
+						creators [1].Backward (grad.Neg(), this);
 					}
 
 //					if (!keepgrads) {
--- UnityProject/Assets/OpenMined/Syft/Tensor/FloatTensor.Ops.cs
+++ UnityProject/Assets/OpenMined/Syft/Tensor/FloatTensor.Ops.cs
@@ -63,17 +63,11 @@ namespace OpenMined.Syft.Tensor
 						});
 			}
 
+		
 			if (autograd) {
-
-				result.InitAutograd ();
-				result.creators.Add (this);
-				result.creators.Add (x);
-				result.creation_op = "add";
-
-				children.Add (result.Id, 0);
-				x.children.Add (result.Id, 0);
-
+				HookAutograd (ref result, ref x, "add_elem");
 			}
+
 
 			return result;
 		}
@@ -287,8 +281,13 @@ namespace OpenMined.Syft.Tensor
 
 			if (dataOnGpu & x.dataOnGpu) {
 				result.Gpu (shader);
-				if (inline) { DivElemGPU_ (x); return this; }
-				else { return DivElemGPU (x, result); }
+				if (inline) { 
+					if(autograd) 
+						throw new InvalidOperationException ("Cannot call inline functions if you intend to run backprop.");
+					DivElemGPU_ (x); 
+					return this; 
+				}
+				else { result = DivElemGPU (x, result); }
 			}
 			else {
 				var nCpu = SystemInfo.processorCount;
@@ -300,6 +299,10 @@ namespace OpenMined.Syft.Tensor
 							}
 						});
 			}
+
+			if(autograd)
+				HookAutograd (ref result, ref x, "div_elem");
+			
 			return result;
 		}
 
@@ -416,15 +419,7 @@ namespace OpenMined.Syft.Tensor
 			}
 
 			if (autograd) {
-
-				result.InitAutograd ();
-				result.creators.Add (this);
-				result.creators.Add (x);
-				result.creation_op = "mul";
-
-				children.Add (result.Id, 0);
-				x.children.Add (result.Id, 0);
-
+				HookAutograd (ref result, ref x, "mul_elem");
 			}
 
 			return result;
@@ -457,9 +452,13 @@ namespace OpenMined.Syft.Tensor
 			FloatTensor result = inline ? this : this.emptyTensorCopy();
 
 			if (dataOnGpu & x.dataOnGpu) {
-				result.Gpu (shader);
-				if (inline) { SubElemGPU_(x); return this;}
-				else { return SubElemGPU (x, result); }
+				if (inline) { 
+					if (autograd)
+						throw new InvalidOperationException ("Cannot call inline functions if you intend to run backprop.");
+					SubElemGPU_(x); 
+					return this;
+				}
+				else { result = SubElemGPU (x, result); }
 			}
 			var nCpu = SystemInfo.processorCount;
 			Parallel.For (0, nCpu, workerId => {
@@ -467,6 +466,11 @@ namespace OpenMined.Syft.Tensor
 						for (var i = size * workerId / nCpu; i < max; i++)
 							result.Data [i] = Data [i] - x.Data [i];
 					});
+
+			if (autograd) {
+				HookAutograd (ref result, ref x, "sub_elem");
+			}
+
 			return result;
 		}
 
