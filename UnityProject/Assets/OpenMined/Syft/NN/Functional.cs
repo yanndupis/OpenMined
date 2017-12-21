30810366d319ee5dbb59eb85270579f3182734cd
Emre Göynügür <emre.goynugur@ozu.edu.tr>
2017-12-22 00:36:34 +0300

    Added numerical stability to Softmax function. (replicated Torch implementation) (#246)

--- UnityProject/Assets/OpenMined/Syft/NN/Functional.cs
+++ UnityProject/Assets/OpenMined/Syft/NN/Functional.cs
@@ -16,32 +16,33 @@ namespace OpenMined.Syft.NN
             if (!input.IsContiguous())
                 throw new NotImplementedException(
                     "Softmax Gradient does not support non-contiguous tensors at the moment!");
-   
+
             //TODO: GPU support
             var gpu = false;
             if (input.DataOnGpu)
-            {   
+            {
                 input.Cpu();
                 gpu = true;
             }
-            
+
             var _dim = (dim == -1) ? input.Shape.Length - 1 : dim;
 
             var outerSize = 1;
             var innerSize = 1;
             var dimSize = input.Shape[_dim];
-            
+
             for (var i = 0; i < _dim; ++i)
                 outerSize *= input.Shape[i];
-            
+
             for (var i = _dim + 1; i < input.Shape.Length; ++i)
                 innerSize *= input.Shape[i];
-            
+
             var dimStride = innerSize;
             var outerStride = dimSize * dimStride;
-            
-            var output= input.Exp();
-            
+
+            var output = input.Copy();
+
+
             var nCpu = SystemInfo.processorCount;
             Parallel.For(0, nCpu, workerId =>
             {
@@ -54,12 +55,26 @@ namespace OpenMined.Syft.NN
                     // works for contiguous!!
                     var index = outerIdx * outerStride + innerIdx;
 
+                    var inputMax = float.MinValue;
+                    for (var d = 0; d < dimSize; d++)
+                    {
+                        if (output.Data[d * dimStride] >= inputMax)
+                            inputMax = output.Data[d * dimStride];
+                    }
+
                     float sum = 0;
                     for (var d = 0; d < dimSize; d++)
-                        sum += output.Data[index + d * dimStride];
+                    {
+                        var z = (float) Math.Exp(output.Data[index + d * dimStride] - inputMax);
+                        output.Data[index + d * dimStride] = z;
+                        sum += z;
+                    }
 
+                    float invSum = 1 / sum;
                     for (var d = 0; d < dimSize; d++)
-                        output.Data[index + d * dimStride] = output.Data[index + d * dimStride] / sum;
+                    {
+                        output.Data[index + d * dimStride] = output.Data[index + d * dimStride] * invSum;
+                    }
                 }
             });
 
@@ -67,8 +82,8 @@ namespace OpenMined.Syft.NN
             {
                 output.Gpu(input.Shader);
             }
-            
-            output = input.HookAutograd(ref output, "softmax-"+ _dim.ToString(), false);
+
+            output = input.HookAutograd(ref output, "softmax-" + _dim.ToString(), false);
 
             return output;
         }
@@ -81,16 +96,16 @@ namespace OpenMined.Syft.NN
             var outerSize = 1;
             var innerSize = 1;
             var dimSize = output.Shape[dim];
-            
+
             for (var i = 0; i < dim; ++i)
                 outerSize *= output.Shape[i];
-            
+
             for (var i = dim + 1; i < output.Shape.Length; ++i)
                 innerSize *= output.Shape[i];
-            
+
             var dimStride = innerSize;
             var outerStride = dimSize * dimStride;
-            
+
             var gradInput = output.emptyTensorCopy();
 
             var nCpu = SystemInfo.processorCount;
@@ -110,7 +125,8 @@ namespace OpenMined.Syft.NN
                         sum += output.Data[index + d * dimStride] * gradOutput.Data[index + d * dimStride];
 
                     for (var d = 0; d < dimSize; d++)
-                        gradInput.Data[index + d * dimStride] = output.Data[index + d * dimStride] * (gradOutput.Data[index + d * dimStride] - sum);
+                        gradInput.Data[index + d * dimStride] =
+                            output.Data[index + d * dimStride] * (gradOutput.Data[index + d * dimStride] - sum);
                 }
             });
 
@@ -118,6 +134,5 @@ namespace OpenMined.Syft.NN
 
             return gradInput;
         }
-        
     }
 }
