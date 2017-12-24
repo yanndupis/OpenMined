2b3c6b9367869a0762fad2cd64adc169e51de4c4
Andrew Trask <liamtrask@gmail.com>
2017-12-24 13:53:17 -0500

    added policy model

---
+++ UnityProject/Assets/OpenMined/Syft/Model/Policy.cs
@@ -0,0 +1,26 @@
+﻿using OpenMined.Network.Controllers;
+using OpenMined.Syft.Tensor;
+
+namespace OpenMined.Syft.Layer
+{
+    public class Policy:Layer
+    {
+        private Layer model;
+        
+        public Policy(SyftController _controller, Layer _model)
+        {
+            init("policy");
+            model = _model;
+        }
+
+        public override FloatTensor Forward(FloatTensor input)
+        {
+            return model.Forward(input);
+        }
+
+        /*public int[] Sample(FloatTensor input)
+        {
+            
+        }*/
+    }
+}
