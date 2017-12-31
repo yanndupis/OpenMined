4de59bf438054245ddd39b0fbdb446a918da7fa5
iamtrask <liamtrask@gmail.com>
2017-12-31 23:14:36 +0000

    bugfix (#284)
    * organized notebooks
    
    * removed redundant notebooks
    
    * added dropout and fixed bug in use of Copy in autograd
    
    * forgot to add the dropout file

---
+++ UnityProject/Assets/OpenMined/Syft/NN/Dropout.cs
@@ -0,0 +1,40 @@
+﻿using OpenMined.Network.Controllers;
+using OpenMined.Syft.Tensor;
+using UnityEngine;
+
+namespace OpenMined.Syft.Layer
+{
+	public class Dropout : Layer
+	{
+
+		private FloatTensor _mask_source;
+		private readonly float rate;
+
+		public Dropout(SyftController _controller, float _rate)
+		{
+			init("dropout");
+
+			this.controller = _controller;
+			this.rate = _rate;
+
+#pragma warning disable 420
+			id = System.Threading.Interlocked.Increment(ref nCreated);
+			controller.addModel(this);
+
+		}
+
+		public override FloatTensor Forward(FloatTensor input)
+		{
+			if (_mask_source == null || input.Size != _mask_source.Size)
+			{
+				_mask_source = input.emptyTensorCopy(hook_graph:false);
+				_mask_source.Fill(1 - rate, inline: true);
+			};
+			
+			FloatTensor output = input.Mul(_mask_source.SampleMask());
+			activation = output.Id;
+			return output;
+			
+		}
+	}
+}
